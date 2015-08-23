e1f848a9cbb64ae5e3f43ed8ec8f1d02a96e8d29
Uice Lu <uicestone@gmail.com>
2015-08-23 20:38:00 +0800

    七牛SDK和私有文件下载

---
+++ wp-content/plugins/qiniu/qiniu.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Qiniu
+Plugin URI: https://github.com/qiniu/php-sdk
+Description: Qiniu Resource (Cloud) Storage SDK for PHP.
+Author: qiniu
+Version: 7.0.4
+Author URI: http://www.qiniu.com/
+*/
+
+require __DIR__ . '/Auth.php';
+require __DIR__ . '/Config.php';
+require __DIR__ . '/functions.php';
+
+use Qiniu\Auth;
+
+function qiniu_get_real_download_url($path){
+	$auth = new Auth(get_option('qiniu_access_key'), get_option('qiniu_secret_key'));
+	return $auth->privateDownloadUrl(get_option('qiniu_host') . $path, 10);
+}
+
