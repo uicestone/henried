bce624d30726e35f0cf63a6d11cbd9c13dd871e4
Uice Lu <uicestone@gmail.com>
2015-10-28 01:13:34 +0800

    去除未使用按钮

--- wp-content/themes/megacourse/header-learning.php
+++ wp-content/themes/megacourse/header-learning.php
@@ -32,9 +32,9 @@ global $title;
 			<h4 class="sm black bold"><?=$title?></h4>
 
 			<ul class="top-nav-list">
-				<li class="prev-course"><a href="#"><i class="icon md-angle-left"></i><span class="tooltip">Prev</span></a></li>
-				<li class="next-course"><a href="#"><i class="icon md-angle-right"></i><span class="tooltip">Next</span></a></li>
-				<li class="outline-learn">
+				<li class="prev-course"><a href="<?=site_url()?>/learning/?course_id=<?=$_GET['course_id']?>&section_id=<?=$_GET['section_id'] - 1?>"><i class="icon md-angle-left"></i><span class="tooltip">上一章</span></a></li>
+				<li class="next-course"><a href="<?=site_url()?>/learning/?course_id=<?=$_GET['course_id']?>&section_id=<?=$_GET['section_id'] + 1?>"><i class="icon md-angle-right"></i><span class="tooltip">下一章</span></a></li>
+				<!--<li class="outline-learn">
 					<a href="#"><i class="icon md-list"></i></a>
 					<div class="list-item-body outline-learn-body">
 						<div class="section-learn-outline">
@@ -55,186 +55,11 @@ global $title;
 									</div>
 									<div class="div-x"><i class="icon md-check-2"></i></div>
 								</li>
-
-								<li>
-									<div class="list-body">
-										<a href="#">
-											<h6>Unit 2</h6>
-											<p>Duis vel ullamcorper mauris, eu pretium felis. Duis gravida laoreet velit.</p>
-										</a>
-									</div>
-									<div class="download">
-										<a href="#"><i class="icon md-download-1"></i></a>
-										<div class="download-ct">
-											<span>Reference 12 mb</span>
-										</div>
-									</div>
-									<div class="div-x"><i class="icon md-check-2"></i></div>
-								</li>
-
-								<li>
-									<div class="list-body">
-										<a href="#">
-											<h6>Unit 3</h6>
-											<p>Duis vel ullamcorper mauris</p>
-										</a>
-									</div>
-									<div class="div-x"><i class="icon md-check-2"></i></div>
-								</li>
-
-								<li>
-									<div class="list-body">
-										<a href="#">
-											<h6>Unit 4</h6>
-											<p>Duis vel ullamcorper mauris,</p>
-										</a>
-									</div>
-									<div class="download">
-										<a href="#"><i class="icon md-download-1"></i></a>
-										<div class="download-ct">
-											<span>Reference 12 mb</span>
-										</div>
-									</div>
-									<div class="div-x"><i class="icon md-check-2"></i></div>
-								</li>
-
-								<li class="o-view">
-									<div class="list-body">
-										<a href="#">
-											<h6>Quizz 1</h6>
-											<p>12 questions</p>
-										</a>
-									</div>
-									<div class="div-x"><i class="icon md-check-2"></i></div>
-								</li>
-
-								<li>
-									<div class="list-body">
-										<a href="#">
-											<h6>Assignment</h6>
-											<p>Scope: All course</p>
-										</a>
-									</div>
-									<div class="div-x"><i class="icon md-check-2"></i></div>
-								</li>
-
 							</ul>
 						</div>
-
-						<div class="section-learn-outline">
-							<h5 class="section-title">Sect 2 : This is long title for the section</h5>
-							<ul class="section-list">
-								<li>
-									<div class="list-body">
-										<a href="#">
-											<h6>Unit 5</h6>
-											<p>Duis vel ullamcorper mauris, eu pretium felis. Duis gravida laoreet velit.</p>
-										</a>
-									</div>
-									<div class="div-x"><i class="icon md-check-2"></i></div>
-								</li>
-							</ul>
-						</div>
-
-					</div>
+					</div>-->
 				</li>
 
-
-
-				<!-- DISCUSSION -->
-				<li class="discussion-learn">
-					<a href="#"><i class="icon md-comments"></i></a>
-					<div class="list-item-body discussion-learn-body">
-						<div class="inner">
-							<div class="form-discussion">
-								<form>
-									<div class="text-title">
-										<input type="text" placeholder="Topic title here">
-									</div>
-									<div class="post-editor text-form-editor">
-										<img src="<?=get_stylesheet_directory_uri()?>/images/editor-demo-1.jpg" alt="">
-										<textarea placeholder="Discussion content"></textarea>
-									</div>
-									<div class="form-submit">
-										<input type="submit" value="Post" class="mc-btn-2 btn-style-2">
-									</div>
-									<h5>3 Topics</h5>
-								</form>
-							</div>
-
-							<ul class="list-discussion">
-
-								<!-- LIST ITEM -->
-								<li>
-									<div class="list-body">
-										<div class="list-content">
-											<cite class="name-author"><a href="#">Anna Molly</a></cite>
-											<p>Morbi nec nisi ante. Quisque lacus ligula, iaculis in elit et, interdum semper quam. Fusce in interdum tortor. Ut sollicitudin lectus dolor eget imperdiet libero pulvinar sit amet</p>
-											<div class="comment-meta">
-											   <a href="#">5 days ago</a>
-											   <a href="#"><i class="icon md-arrow-up"></i>13</a>
-											   <a href="#"><i class="icon md-arrow-down"></i>25</a>
-											   <a href=""><i class="icon md-back"></i>384 replies</a>
-											</div>
-										</div>
-									</div>
-								</li>
-								<!-- END / LIST ITEM -->
-
-								<!-- LIST ITEM -->
-								<li>
-									<div class="list-body">
-										<div class="list-content">
-											<cite class="name-author"><a href="#">Anna Molly</a></cite>
-											<p>Title of topic shown only. When user click on topic it show full content and discussion below</p>
-											<div class="comment-meta">
-											   <a href="#">5 days ago</a>
-											   <a href="#"><i class="icon md-arrow-up"></i>13</a>
-											   <a href="#"><i class="icon md-arrow-down"></i>25</a>
-											   <a href=""><i class="icon md-back"></i>384 replies</a>
-											</div>
-										</div>
-									</div>
-								</li>
-								<!-- END / LIST ITEM -->
-
-								<!-- LIST ITEM -->
-								<li>
-									<div class="list-body">
-										<div class="list-content">
-											<cite class="name-author"><a href="#">Anna Molly</a></cite>
-											<p>Title of topic shown only. When user click on topic it show full content and discussion below</p>
-											<div class="comment-meta">
-											   <a href="#">5 days ago</a>
-											   <a href="#"><i class="icon md-arrow-up"></i>13</a>
-											   <a href="#"><i class="icon md-arrow-down"></i>25</a>
-											   <a href=""><i class="icon md-back"></i>384 replies</a>
-											</div>
-										</div>
-									</div>
-								</li>
-								<!-- END / LIST ITEM -->
-							</ul>
-						</div>
-					</div>
-				</li>
-
-				<!-- NOTE LEARN -->
-				<li class="note-learn">
-					<a href="#"><i class="icon md-file"></i></a>
-					<div class="list-item-body note-learn-body">
-						<div class="note-title">
-							<h5>Note</h5>
-							<a href="#"><i class="icon md-share"></i></a>
-						</div>
-						<div contenteditable="true" class="note-body">
-							Start writing here
-						</div>
-					</div>
-				</li>
-				<li class="backpage">
-					<a href="index.html"><i class="icon md-back"></i></a>
-				</li>
 			</ul>
 
 		</div>
