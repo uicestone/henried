dd3f66fee831cf9c7c6771a97838e76f294cb634
Uice Lu <uicestone@gmail.com>
2015-11-23 12:16:00 +0800

    404页面

--- wp-content/themes/megacourse/index.php
+++ wp-content/themes/megacourse/index.php
@@ -1,2 +1,10 @@
 <?php get_header(); ?>
+<section class="sub-banner sub-banner-course">
+	<div class="awe-static bg-sub-banner-course"></div>
+	<div class="container">
+		<div class="sub-banner-content">
+			<h2 class="text-center">Nothing Found Here. KEEP RUNNING!</h2>
+		</div>
+	</div>
+</section>
 <?php get_footer(); ?>
