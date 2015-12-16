559c7569fffc44645290a83ae2bc93b10541de8c
Uice Lu <uicestone@gmail.com>
2015-12-17 01:11:33 +0800

    fix: 两栏新闻对齐问题

--- wp-content/themes/megacourse/functions.php
+++ wp-content/themes/megacourse/functions.php
@@ -15,14 +15,14 @@ add_action('init', function(){
 	wp_register_style('font-awesome', get_stylesheet_directory_uri() . '/css/library/font-awesome.min.css', array(), '4.2.0');
 	wp_register_style('owl.carousel', get_stylesheet_directory_uri() . '/css/library/owl.carousel.css', array(), '1.18');
 	wp_register_style('md-font', get_stylesheet_directory_uri() . '/css/md-font.css', array());
-	wp_register_style('style', get_stylesheet_directory_uri() . '/style.css', array(), '2015-11-08');
+	wp_register_style('style', get_stylesheet_directory_uri() . '/style.css', array(), '2015-12-17');
 	
 	wp_register_script('bootstrap', get_stylesheet_directory_uri() . '/js/library/bootstrap.min.js', array('jquery'), '3.2.0', true);
 	wp_register_script('jquery.owl.carousel', get_stylesheet_directory_uri() . '/js/library/jquery.owl.carousel.js', array('jquery'), '1.3.2', true);
 	wp_register_script('jquery.appear', get_stylesheet_directory_uri() . '/js/library/jquery.appear.min.js', array('jquery'), null, true);
 	wp_register_script('jquery.easing', get_stylesheet_directory_uri() . '/js/library/jquery.easing.min.js', array('jquery'), null, true);
 	wp_register_script('perfect-scrollbar', get_stylesheet_directory_uri() . '/js/library/perfect-scrollbar.min.js', array('jquery'), '0.5.6', true);
-	wp_register_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
+	wp_register_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'), '2015-12-17', true);
 });
 
 add_action('wp_enqueue_scripts', function(){
