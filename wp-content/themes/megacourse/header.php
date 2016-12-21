c82e13d02d45b0571da58c371bb442e5c3c255f4
Uice Lu <uicestone@gmail.com>
2016-12-22 01:22:14 +0800

    主题颜色改为Henried红

--- wp-content/themes/megacourse/header.php
+++ wp-content/themes/megacourse/header.php
@@ -33,7 +33,7 @@
 				<div class="container">
 
 					<!-- LOGO -->
-					<div class="logo"><a href="<?=site_url()?>">Henried</a></div>
+					<div class="logo"><a href="<?=site_url()?>"><img src="<?=get_stylesheet_directory_uri()?>/images/logo.png"></a></div>
 					<!-- END / LOGO -->
 
 					<!-- NAVIGATION -->
